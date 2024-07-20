8253de5a612085028b5cdf628b077fa4df5658e6
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-07-21 00:04:05 +0900

    feat:  add iOS app

---
+++ ios/flutterModule/flutterModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutterModuleApp.swift
+//  flutterModule
+//
+//  Created by Kosuke Saigusa on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct flutterModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
