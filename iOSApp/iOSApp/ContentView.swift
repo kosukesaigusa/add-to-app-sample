fda4f87d8f49457172d66b64190f469bf834ccc9
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-07-22 16:38:51 +0900

    feat:  enable to build iOS app and show Flutter

--- iOSApp/iOSApp/ContentView.swift
+++ iOSApp/iOSApp/ContentView.swift
@@ -1,24 +1,55 @@
+//import SwiftUI
+//import Flutter
 //
-//  ContentView.swift
-//  iOSApp
+//struct FlutterViewControllerRepresentable: UIViewControllerRepresentable {
+//  // Flutter dependencies are passed in through the view environment.
+//  @Environment(FlutterDependencies.self) var flutterDependencies
+//  
+//  func makeUIViewController(context: Context) -> some UIViewController {
+//    return FlutterViewController(
+//      engine: flutterDependencies.flutterEngine,
+//      nibName: nil,
+//      bundle: nil)
+//  }
+//  
+//  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+//}
 //
-//  Created by Kosuke Saigusa on 2024/07/22.
+//struct ContentView: View {
 //
+//  var body: some View {
+//    NavigationStack {
+//      NavigationLink("My Flutter Feature") {
+//        FlutterViewControllerRepresentable()
+//      }
+//    }
+//  }
+//}
 
 import SwiftUI
+import Flutter
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct FlutterViewControllerRepresentable: UIViewControllerRepresentable {
+  // Access the AppDelegate through the view environment.
+  @Environment(AppDelegate.self) var appDelegate
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    return FlutterViewController(
+      engine: appDelegate.flutterEngine,
+      nibName: nil,
+      bundle: nil)
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+
+  var body: some View {
+    NavigationStack {
+      NavigationLink("My Flutter Feature") {
+        FlutterViewControllerRepresentable()
+      }
+    }
+  }
 }
--- iOSApp/iOSApp/iOSAppApp.swift
+++ iOSApp/iOSApp/iOSAppApp.swift
@@ -1,17 +1,59 @@
+//import SwiftUI
+//import Flutter
+//// The following library connects plugins with iOS platform code to this app.
+//import FlutterPluginRegistrant
 //
-//  iOSAppApp.swift
-//  iOSApp
+//@Observable
+//class FlutterDependencies {
+// let flutterEngine = FlutterEngine(name: "my flutter engine")
+// init() {
+//   // Runs the default Dart entrypoint with a default Flutter route.
+//   flutterEngine.run()
+//   // Connects plugins with iOS platform code to this app.
+//   GeneratedPluginRegistrant.register(with: self.flutterEngine);
+// }
+//}
 //
-//  Created by Kosuke Saigusa on 2024/07/22.
-//
+//@main
+//struct MyApp: App {
+//   // flutterDependencies will be injected through the view environment.
+//   @State var flutterDependencies = FlutterDependencies()
+//   var body: some Scene {
+//     WindowGroup {
+//       ContentView()
+//         .environment(flutterDependencies)
+//     }
+//   }
+//}
 
 import SwiftUI
+import Flutter
+import FlutterPluginRegistrant
+
+@Observable
+class AppDelegate: FlutterAppDelegate {
+  let flutterEngine = FlutterEngine(name: "my flutter engine")
+
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      // Runs the default Dart entrypoint with a default Flutter route.
+      flutterEngine.run();
+      // Used to connect plugins (only if you have plugins with iOS platform code).
+      GeneratedPluginRegistrant.register(with: self.flutterEngine);
+      return true;
+    }
+}
 
 @main
-struct iOSAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct MyApp: App {
+  // Use this property wrapper to tell SwiftUI
+  // it should use the AppDelegate class for the application delegate
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+  var body: some Scene {
+      WindowGroup {
+        ContentView()
+      }
+  }
 }
