cd79eaaca7b044ae385d1c702a6cef83cf267dd8
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-07-22 15:18:36 +0900

    build:  add iOSApp

---
+++ iOSApp/iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by Kosuke Saigusa on 2024/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
